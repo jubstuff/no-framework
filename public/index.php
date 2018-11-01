f1dbc75759b484e5da41852f4be82f550b1c2bd5
Giustino Borzacchiello <giustinob@gmail.com>
2018-11-01 23:09:07 +0100

    Add Router, RequestHandler and Emitter

--- public/index.php
+++ public/index.php
@@ -2,6 +2,46 @@
 
 declare(strict_types=1);
 
+use DI\ContainerBuilder;
+use function DI\create;
+use function DI\get;
+use ExampleApp\HelloWorld;
+use function FastRoute\simpleDispatcher;
+use Middlewares\FastRoute;
+use Middlewares\RequestHandler;
+use Relay\Relay;
+use Zend\Diactoros\Response;
+use Zend\Diactoros\ServerRequestFactory;
+use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
+
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-echo 'Hello, World!';
+// init container
+$containerBuilder = new ContainerBuilder();
+$containerBuilder->useAutowiring(false);
+$containerBuilder->useAnnotations(false);
+$containerBuilder->addDefinitions([
+    HelloWorld::class => create(HelloWorld::class)->constructor(get('Foo'), get('Response')),
+    'Foo' => 'bar',
+    'Response' => function () {
+        return new Response();
+    }
+]);
+
+$container = $containerBuilder->build();
+
+$middlewareQueue = [];
+
+$routes = simpleDispatcher(function (\FastRoute\RouteCollector $r) {
+    $r->get('/hello/{message}', HelloWorld::class);
+});
+// FastRoute determines if a request is valid and can actually be handled by the application
+$middlewareQueue[] = new FastRoute($routes);
+// RequestHandler sends Request to the handler configured for that route in the routes definition.
+$middlewareQueue[] = new RequestHandler($container);
+
+$requestHandler = new Relay($middlewareQueue);
+$response = $requestHandler->handle(ServerRequestFactory::fromGlobals());
+
+$emitter = new SapiEmitter();
+return $emitter->emit($response);
---
+++ src/HelloWorld.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace ExampleApp;
+
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class HelloWorld
+{
+    private $foo;
+
+    private $response;
+
+    public function __construct(string $foo, ResponseInterface $response)
+    {
+        $this->foo = $foo;
+        $this->response = $response;
+    }
+
+    public function __invoke(RequestInterface $request): ResponseInterface
+    {
+        $response = $this->response->withHeader('Content-Type', 'text/html');
+        $response->getBody()->write("<html><head></head><body>Hello, {$this->foo} world!</body></html>");
+
+        return $response;
+    }
+
+}
